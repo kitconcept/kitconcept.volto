0260675ea6b72aebc70a8c78fd8e0e0af5f35d49
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-11-02 12:06:48 +0100

    Back to development: 3.0.0a5
    Former-commit-id: 340d5e2436b6925142b62a5fb38c96367486d5db

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="kitconcept.volto",
-    version="3.0.0a4",
+    version="3.0.0a5.dev0",
     description="Volto integration add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
